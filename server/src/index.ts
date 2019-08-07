98bc4b48fa42eb8973e42465bd59fb9ef96b41e7
David Hwang <alwaysmile4u@gmail.com>
2019-08-07 17:53:14 +0900

    set the types for express req and res / set .env file

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,15 +3,16 @@ import express from "express";
 import dotenv from 'dotenv'
 
 dotenv.config()
+const status = process.env.NODE_ENV
 const app: express.Application = express();
 const port: string | number = process.env.SERVER_PORT || 4040
 
 app.use(express.static(path.join(__dirname, '../dist/views')))
 
-app.get("/", (req, res) => {
+app.get("/", (req: express.Request, res: express.Response) => {
   res.sendFile(path.join(__dirname, '../dist/views/index.html'))
 });
 
 app.listen(port, () => {
-  console.log(`server started at port: ${port}`);
+  console.log(`status: ${status}, server started at port: ${port}`);
 });
